66e62057b1aa7cbff09fd1c7659d08731fdc2bf0
Pectojin <Pectojilien@gmail.com>
2014-02-16 18:15:29 +0100

    Some comments changed

--- Address Parsing/test/address/parsing/AddressParsingTest.java
+++ Address Parsing/test/address/parsing/AddressParsingTest.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package address.parsing;
 
 import java.sql.Connection;
@@ -16,11 +10,11 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-        
+
 /**
- *
- * @author pecto
- */
+*
+* @author Mikael Lindemann Jepsen <mlin@itu.dk>
+*/
 public class AddressParsingTest {
     Connection con;
 
