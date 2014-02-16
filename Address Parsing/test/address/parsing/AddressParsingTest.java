7bf34c864d707f33d3e5764642a44d6ef9bc85ef
Pectojin <pectojilien@gmail.com>
2014-02-16 15:56:30 +0100

    Added junit testing class. Fixed a bug causing the code to always replace "sal" in all words rendering "absalonvej" and similar words to return malformed address.

---
+++ Address Parsing/test/address/parsing/AddressParsingTest.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package address.parsing;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+        
+/**
+ *
+ * @author pecto
+ */
+public class AddressParsingTest {
+    Connection con;
+
+    public AddressParsingTest() {
+
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    try {
+            if (con == null || con.isValid(5)) {
+                con = DriverManager.getConnection("jdbc:mysql://mysql.itu.dk/GroupE", "GroupE", "GroupE");
+                // For local database
+                // con = DriverManager.getConnection("jdbc:mysql://localhost:3306/GroupE", "root", "GroupE");
+
+            }
+        } catch (SQLException ex) {
+            //Do something.
+        }
+        
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of parseAddress method, of class AddressParsing.
+     * @throws java.lang.Exception
+    */
+    @Test
+    public void testParseAddress() throws Exception {        
+        try {
+            PreparedStatement ps = con.prepareStatement(
+                    "SELECT * FROM `roadnames`"); // WHERE `roadname`='Abrikosvej'
+            ResultSet rs = ps.executeQuery();
+                        
+            int i = -1;
+            while (rs.next()) {
+                i++;
+                String[] parsedAddress = AddressParsing.parseAddress(rs.getString(1));                 
+                System.out.println(rs.getString(1) + " " + parsedAddress[0]);
+                if (!rs.getString(1).equals(parsedAddress[0])) {
+                    System.out.println(parsedAddress[0] + " line: " + (i+1) + " should have read: " + rs.getString(1));
+                }
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }                    
+    }
+
+}
