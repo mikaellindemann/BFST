bcbbaf7050eda8c85f20790ebd524a4309f67fce
Mikael <Mikael@192.168.1.45>
2014-03-05 08:06:55 +0100

    Change a label from "Zoom to 100%" to "Show entire map". Also getting static-variables from class instead of object. The control-panel is now added before packing the frame.
    This also means that the preferredSize-method of Map has been changed to fit on my screen ;)

--- Map/src/dk/itu/groupe/GUI.java
+++ Map/src/dk/itu/groupe/GUI.java
@@ -10,9 +10,6 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
 import java.io.IOException;
 import javax.swing.AbstractAction;
 import javax.swing.BoxLayout;
@@ -28,52 +25,52 @@ import javax.swing.KeyStroke;
  * @author Peter
  */
 public class GUI extends JComponent {
-
+    
     JLabel label;
     private String yTrack;
     private String xTrack;
-
+    
     public void gui() throws IOException {
-
+        
         final Map loader = new Map();
-
+        
         JPanel panel = new JPanel();
         JPanel remotePanel = new JPanel();
         JPanel remoteLayoutLeft = new JPanel();
         JPanel remoteLayoutRight = new JPanel();
         JFrame frame = new JFrame();
-
+        
         frame.setLayout(new BorderLayout());
         frame.add(panel, BorderLayout.SOUTH);
         frame.add(remotePanel, BorderLayout.EAST);
         remoteLayoutLeft.setLayout(new BoxLayout(remoteLayoutLeft, BoxLayout.Y_AXIS));
         remoteLayoutRight.setLayout(new BoxLayout(remoteLayoutRight, BoxLayout.Y_AXIS));
-
+        
         remoteLayoutLeft.setMinimumSize(new Dimension(100, 120));
         remoteLayoutLeft.setPreferredSize(new Dimension(100, 120));
-
+        
         remoteLayoutRight.setMinimumSize(new Dimension(100, 120));
         remoteLayoutRight.setPreferredSize(new Dimension(100, 120));
-
+        
         label = new JLabel("x:" + xTrack + "| y:" + yTrack);
         panel.add(label);
-
+        
         remotePanel.add(remoteLayoutLeft);
         remotePanel.add(remoteLayoutRight);
-
-        JButton button = new JButton("Zoom to 100%");
+        
+        JButton button = new JButton("Show entire map");
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent event) {
                 loader.reset();
             }
         });
-
+        
         JButton buttonUp = new JButton("Go up(↑)");
         buttonUp.setMaximumSize(new Dimension(100, 40));
 
         //Add keybind function
-        buttonUp.getInputMap(buttonUp.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_UP, 0), "buttonUp");
+        buttonUp.getInputMap(JButton.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_UP, 0), "buttonUp");
         buttonUp.getActionMap().put("buttonUp", new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -88,12 +85,12 @@ public class GUI extends JComponent {
                 loader.goUp();
             }
         });
-
+        
         JButton buttonLeft = new JButton("Go left(←)");
         buttonLeft.setMaximumSize(new Dimension(100, 40));
 
         //Add keybind function
-        buttonLeft.getInputMap(buttonLeft.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, 0), "buttonLeft");
+        buttonLeft.getInputMap(JButton.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, 0), "buttonLeft");
         buttonLeft.getActionMap().put("buttonLeft", new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -108,12 +105,12 @@ public class GUI extends JComponent {
                 loader.goLeft();
             }
         });
-
+        
         JButton buttonRight = new JButton("Go right(→)");
         buttonRight.setMaximumSize(new Dimension(100, 40));
 
         //Add keybind function
-        buttonRight.getInputMap(buttonRight.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, 0), "buttonRight");
+        buttonRight.getInputMap(JButton.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, 0), "buttonRight");
         buttonRight.getActionMap().put("buttonRight", new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -128,12 +125,12 @@ public class GUI extends JComponent {
                 loader.goRight();
             }
         });
-
+        
         JButton buttonDown = new JButton("Go down(↓)");
         buttonDown.setMaximumSize(new Dimension(100, 40));
 
         //Add keybind function
-        buttonDown.getInputMap(buttonDown.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, 0), "buttonDown");
+        buttonDown.getInputMap(JButton.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, 0), "buttonDown");
         buttonDown.getActionMap().put("buttonDown", new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -148,12 +145,12 @@ public class GUI extends JComponent {
                 loader.goDown();
             }
         });
-
+        
         JButton buttonZoomIn = new JButton("Zoom in(+)");
         buttonZoomIn.setMaximumSize(new Dimension(100, 40));
 
         //Add keybind function
-        buttonZoomIn.getInputMap(buttonZoomIn.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_PLUS, 0), "buttonZoomIn");
+        buttonZoomIn.getInputMap(JButton.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_PLUS, 0), "buttonZoomIn");
         buttonZoomIn.getActionMap().put("buttonZoomIn", new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -168,12 +165,12 @@ public class GUI extends JComponent {
                 loader.ZoomIn();
             }
         });
-
+        
         JButton buttonZoomOut = new JButton("Zoom out(-)");
         buttonZoomOut.setMaximumSize(new Dimension(100, 40));
 
         //Add keybind function
-        buttonZoomOut.getInputMap(buttonZoomOut.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_MINUS, 0), "buttonZoomOut");
+        buttonZoomOut.getInputMap(JButton.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_MINUS, 0), "buttonZoomOut");
         buttonZoomOut.getActionMap().put("buttonZoomOut", new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -195,21 +192,21 @@ public class GUI extends JComponent {
         remoteLayoutRight.add(buttonRight);
         remoteLayoutLeft.add(buttonZoomIn);
         remoteLayoutRight.add(buttonZoomOut);
-
+        
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().setLayout(new BorderLayout());
         frame.getContentPane().add(loader, BorderLayout.CENTER);
         frame.getContentPane().addMouseListener(loader);
         frame.getContentPane().addMouseMotionListener(loader);
-
+        
+        frame.getContentPane().add(remotePanel, BorderLayout.EAST);
+        frame.getContentPane().add(panel, BorderLayout.SOUTH);
+        
         frame.pack();
         frame.repaint();
         frame.setVisible(true);
-
-        panel.setVisible(true);
-        remotePanel.setVisible(true);
-        frame.getContentPane().add(remotePanel, BorderLayout.EAST);
-        frame.getContentPane().add(panel, BorderLayout.SOUTH);
-
+        
+        
+        
     }
 }
